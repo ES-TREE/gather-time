691882987ef58d2065098c3685ab0f81b2835f05
ixio0330 <ixio0330@gmail.com>
2025-02-22 21:01:08 +0900

    🔧 라우팅 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+// 라우팅
+import routes from "./routes";
+
 export default function App() {
-  return null;
+  return (
+    <Router>
+      <main>
+        <Routes>
+          {routes.map(({ id, path, element }) => (
+            <Route key={`page-${id}`} path={path} element={element} />
+          ))}
+        </Routes>
+      </main>
+    </Router>
+  );
 }
---
+++ src/routes/index.jsx
@@ -0,0 +1,12 @@
+import CreateEventPage from "../pages/CreateEventPage";
+
+const routes = [
+  {
+    id: "create-event-page",
+    name: "이벤트 생성",
+    path: "/",
+    element: <CreateEventPage />,
+  },
+];
+
+export default routes;
