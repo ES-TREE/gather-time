ab37c85a25990d501c4a3b1031680e10c7a4d05b
ixio0330 <ixio0330@gmail.com>
2025-02-22 20:35:23 +0900

    🔧 tailwind 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#eefbf3",
+          100: "#d6f5e0",
+          200: "#afebc5",
+          300: "#7cd9a5",
+          400: "#57c78c",
+          500: "#23a666",
+          600: "#158651",
+          700: "#116b42",
+          800: "#105537",
+          900: "#0e462e",
+          950: "#07271b",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
