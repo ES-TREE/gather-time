991c94eca294d3d993d7b21bd3ea58f643fba479
ixio0330 <ixio0330@gmail.com>
2025-02-22 23:14:08 +0900

    💡 이벤트 생성 페이지 구현 로직 주석

--- src/pages/CreateEventPage.jsx
+++ src/pages/CreateEventPage.jsx
@@ -2,6 +2,12 @@ import Button from "../components/common/Button"
 import Input from "../components/common/Input"
 
 export default function CreateEventPage() {
+  /**
+   * 구현 로직
+   * 1. 이벤트 이름 상태와 선택한 날짜 상태로 이벤트 생성
+   * 2. 이벤트 생성 후 이벤트 페이지(/uid)로 이동
+   */
+
   return (
     <section>
       <Input label="이벤트 이름" placeholder="이벤트 이름을 입력해주세요." />
