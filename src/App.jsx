ae13316c56b130a7c94065b761c1f506b9b8886b
ixio0330 <ixio0330@gmail.com>
2025-02-23 21:53:57 +0900

    💄 헤더 상단에 고정

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import routes from "./routes"
 export default function App() {
   return (
     <Router>
-      <div className="flex items-center justify-center border-b bg-white">
+      <div className="fixed left-0 top-0 flex w-full items-center justify-center border-b bg-white">
         <Header />
       </div>
-      <main className="m-auto max-w-screen-sm bg-white p-5">
+      <main className="m-auto mt-14 max-w-screen-sm bg-white p-5">
         <Routes>
           {routes.map(({ id, path, element }) => (
             <Route key={`page-${id}`} path={path} element={element} />
