31fef4f2cb504fe5d78ae278cec6aa27e9ae86de
ixio0330 <ixio0330@gmail.com>
2025-02-24 09:09:44 +0900

    💄 모달 최대 넓이 지정

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -7,7 +7,9 @@ export default function Modal({ open, children, className = "" }) {
 
   return (
     <article className="fixed left-0 top-0 z-50 flex h-screen w-full items-center justify-center bg-stone-900 bg-opacity-40 p-5">
-      <div className={`relative w-full rounded-2xl bg-white p-5 ${className}`}>
+      <div
+        className={`relative w-full max-w-screen-sm rounded-2xl bg-white p-5 ${className}`}
+      >
         {children}
       </div>
     </article>
