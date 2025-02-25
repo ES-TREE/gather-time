da569f546b52d93dd68eb035ac6b2242335adaa0
ixio0330 <ixio0330@gmail.com>
2025-02-25 21:17:20 +0900

    👔 이벤트 이름 상태 연결

--- src/pages/CreateEventPage.jsx
+++ src/pages/CreateEventPage.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react"
 import FixedBottomButton from "../components/button/FixedBottomButton"
 import DateRangeCalendar from "../components/calendar/DateRangeCalendar"
 import Input from "../components/common/Input"
 
 export default function CreateEventPage() {
-  /**
-   * 구현 로직
-   * 1. 이벤트 이름 상태와 선택한 날짜 상태로 이벤트 생성
-   * 2. 이벤트 생성 후 이벤트 페이지(/uid)로 이동
-   */
+  // 이벤트 이름
+  const [eventName, setEventName] = useState("")
+
+  // TODO 캘린더 날짜 데이터
+
+  const handleCreateEvent = async (e) => {
+    e.preventDefault()
+
+    /**
+     * 구현 로직
+     * 1. 이벤트 이름 상태와 선택한 날짜 상태로 이벤트 생성
+     * 2. 이벤트 생성 후 이벤트 페이지(/uid)로 이동
+     */
+  }
 
   return (
-    <form className="space-y-5">
-      <Input label="이벤트 이름" placeholder="이벤트 이름을 입력해주세요." />
+    <form className="flex flex-col space-y-5" onSubmit={handleCreateEvent}>
+      <Input
+        value={eventName}
+        onChange={(e) => setEventName(e.target.value)}
+        label="이벤트 이름"
+        placeholder="이벤트 이름을 입력해주세요."
+      />
 
       <section>
         <div className="space-y-1">
